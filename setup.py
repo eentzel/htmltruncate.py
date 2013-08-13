28a455d4b030558b1e23fbad3c97e8442973206e
Martin Ortbauer <mortbauer@gmail.com>
2013-08-13 23:02:58 +0200

    added a setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from setuptools import setup
+
+setup(name='htmltruncate',
+      version='1.0',
+      description='To truncate html content meaningfully',
+      author='Martin Ortbauer',
+      author_email='mortbauer@gmail.com',
+      url='https://mortbauer.com',
+      py_modules = ['htmltruncate']
+      )
