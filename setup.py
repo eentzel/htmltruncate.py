65a5518c70646032be5e3850428473c3bf72a691
Martin Ortbauer <mortbauer@gmail.com>
2013-08-15 13:58:11 +0200

    updated metadata

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ from setuptools import setup
 setup(name='htmltruncate',
       version='1.0',
       description='To truncate html content meaningfully',
-      author='Martin Ortbauer',
-      author_email='mortbauer@gmail.com',
-      url='https://mortbauer.com',
+      author='Eric Entzel',
+      author_email='eric@ubermac.net',
+      url='https://github.com/eentzel/htmltruncate.py',
       py_modules = ['htmltruncate']
       )
